1e131e47e333d9344106e96d0a3534aa178ce73f
zjiajun <zhujiajun.me@gmail.com>
2020-02-05 19:19:50 +0800

    add XML Serializer

---
+++ src/main/java/hawthorn/serializer/xml/XMLSerializer.java
@@ -0,0 +1,34 @@
+package hawthorn.serializer.xml;
+
+import hawthorn.serializer.Serializer;
+
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+/**
+ * @author zhujiajun
+ * @version 1.0
+ * @since 2020/2/5 19:10
+ */
+public class XMLSerializer implements Serializer {
+
+    @Override
+    public <T> byte[] serialize(T obj) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        XMLEncoder xmlEncoder = new XMLEncoder(baos);
+        xmlEncoder.writeObject(obj);
+        xmlEncoder.close();
+        return baos.toByteArray();
+    }
+
+    @Override
+    public <T> T deserialize(byte[] data, Class<T> clazz) {
+        ByteArrayInputStream bais = new ByteArrayInputStream(data);
+        XMLDecoder xmlDecoder = new XMLDecoder(bais);
+        Object object = xmlDecoder.readObject();
+        xmlDecoder.close();
+        return (T) object;
+    }
+}
